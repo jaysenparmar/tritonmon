c09f96c9e58cfbdb7a4879c43d35f57cdbcae424
Anurag <akalavakunta@gmail.com>
2014-11-19 12:37:54 -0800

    working on party screen with drag-sort listview

--- app/src/main/java/com/tritonmon/activity/Party.java
+++ app/src/main/java/com/tritonmon/activity/Party.java
@@ -4,17 +4,67 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
-import com.tritonmon.activity.R;
+import com.mobeta.android.dslv.DragSortController;
+import com.mobeta.android.dslv.DragSortListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Party extends Activity {
+
+    private DragSortListView listView;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party);
+
+        listView = (DragSortListView) findViewById(R.id.listview);
+        String[] names = getResources().getStringArray(R.array.random_names);
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(names));
+        adapter = new ArrayAdapter<String>(this, R.layout.item_layout, R.id.artist_name_textview, list);
+        listView.setAdapter(adapter);
+        listView.setDropListener(onDrop);
+        listView.setRemoveListener(onRemove);
+
+        DragSortController controller = new DragSortController(listView);
+        controller.setDragHandleId(R.id.drag_handle);
+        //controller.setClickRemoveId(R.id.);
+        controller.setRemoveEnabled(false);
+        controller.setSortEnabled(true);
+        controller.setDragInitMode(1);
+        //controller.setRemoveMode(removeMode);
+
+        listView.setFloatViewManager(controller);
+        listView.setOnTouchListener(controller);
+        listView.setDragEnabled(true);
     }
 
+    private DragSortListView.DropListener onDrop = new DragSortListView.DropListener()
+    {
+        @Override
+        public void drop(int from, int to)
+        {
+            if (from != to)
+            {
+                String item = adapter.getItem(from);
+                adapter.remove(item);
+                adapter.insert(item, to);
+            }
+        }
+    };
+
+    private DragSortListView.RemoveListener onRemove = new DragSortListView.RemoveListener()
+    {
+        @Override
+        public void remove(int which)
+        {
+            adapter.remove(adapter.getItem(which));
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
